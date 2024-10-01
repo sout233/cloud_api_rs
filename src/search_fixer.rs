d4a23282cfa5e6eb53c0f8e4b78eec5b988e33e4
sout233 <2273466101@qq.com>
2024-10-01 06:54:31 +0000

    re init

---
+++ src/search_fixer.rs
@@ -0,0 +1,62 @@
+use serde::{Deserialize, Serialize};
+
+use crate::search::SearchApiResponse;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Artist {
+    id: u64,
+    name: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Album {
+    id: u64,
+    name: String,
+    #[serde(rename = "picUrl")]
+    pic_url: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Song {
+    name: String,
+    id: u64,
+    ar: Vec<Artist>,
+    al: Album,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct SongDetailResponse {
+    songs: Vec<Song>,
+}
+
+pub(crate) async fn fix_cover_url(
+    predata: &mut SearchApiResponse,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let ids = predata
+        .result
+        .songs
+        .iter()
+        .map(|s| s.id.to_string())
+        .collect::<Vec<String>>();
+
+    let ids = format!("{}", ids.join(","));
+
+    println!("ids: {:#?}", ids);
+    let url = format!("https://re-wyy-api.sout.eu.org/song/detail?ids={}", ids);
+
+    let response = reqwest::get(&url).await?;
+    let result: SongDetailResponse = response.json().await?;
+
+    for song in result.songs {
+        predata
+            .result
+            .songs
+            .iter_mut()
+            .find(|s| s.id == song.id)
+            .unwrap()
+            .album
+            .cover_url = Some(song.al.pic_url.clone());
+    }
+
+    Ok(())
+}
